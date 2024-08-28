c20d1c4032aaa7c0a6fe3fbb750d2884aa59fcd1
yanxin Peng <peterpen@usc.edu>
2024-08-29 00:58:44 +0800

    now starting to modify checkpointing, really complicated

--- megatron/checkpointing.py
+++ megatron/checkpointing.py
@@ -15,6 +15,7 @@ from .global_vars import get_args
 from .utils import (unwrap_model,
                     print_rank_0)
 from scipy.sparse import csr_matrix
+from sparse_utils import generate_bitmask, generate_sparse_delta_with_bitmask, reconstruct_checkpoint_with_bitmask, model_diff
 # from ...adamint8bit.kmeans_experiment.exp_w.clusterer import Clusterer
 # from ...adamint8bit.kmeans_experiment.exp_w.quantizer import Quantizer, FP16Quantizer, Int8BlockwiseQuantizer, Int8DynamicQuantizer, Int8NaiveQuantizer
 
@@ -244,74 +245,6 @@ def get_rng_state():
 
     return rng_state_list
 
-def model_diff(base_model, new_model, diff_state_dict):
-    # this function is used to calculate the difference between the base model and the new model
-    # since the two models are of the same format, we iterate through them together, and when reaching a tensor, we calculate the difference between the two tensors
-    for key in base_model.keys():
-        if key in new_model.keys():
-            if isinstance(base_model[key], dict) and isinstance(new_model[key], dict):
-                diff_state_dict[key] = {}
-                model_diff(base_model[key], new_model[key], diff_state_dict[key])
-            elif isinstance(base_model[key], torch.Tensor) and isinstance(new_model[key], torch.Tensor):
-                diff_state_dict[key] = new_model[key] - base_model[key]
-            # another situation is that both base_model[key] and new_model[key] are NoneType, in this case, skip this iteration
-            elif base_model[key] is None and new_model[key] is None:
-                diff_state_dict[key] = None
-            else:
-                raise ValueError(f"Mismatched types for key '{key}' in base and new model, base has the type {type(base_model[key])} and new has the type {type(new_model[key])}")
-        else:
-            raise ValueError(f"Key '{key}' not found in new model")
-
-
-def compress_indices(indices, dtype=torch.int32):
-    # Compress indices to a specified lower-bit integer type
-    return indices.to(dtype)
-
-def decompress_indices(indices, dtype=torch.int64):
-    # Decompress indices back to int64
-    return indices.to(dtype)
-
-def sparsification(diff_model, sparse_model):
-    # This function is used to perform sparsification on the diff_model
-    # We need to iterate through the diff_model and perform sparsification on the tensors
-    for key in diff_model:
-        if isinstance(diff_model[key], dict):
-            sparse_model[key] = {}
-            sparsification(diff_model[key], sparse_model[key])
-        elif isinstance(diff_model[key], torch.Tensor):
-            # Perform sparsification using COO format
-            tensor = diff_model[key].to_sparse()
-            indices = tensor.indices()
-            values = tensor.values()
-            indices_numpy = indices.cpu().numpy()
-            # Compress indices for storage or transmission
-            compressed_indices = indices_numpy.astype(np.uint16)
-            # compressed_indices = compress_indices(indices, dtype=torch.uint16)
-            # Store the compressed indices and values
-            sparse_model[key] = {
-                "size": tensor.size(),
-                "compressed_indices": compressed_indices,
-                "values": values
-            }
-
-        elif diff_model[key] is None:
-            sparse_model[key] = None
-
-
-def csr_sparsification(diff_model, sparse_model):
-    for key in diff_model:
-        if isinstance(diff_model[key], dict):
-            sparse_model[key] = {}
-            sparsification(diff_model[key], sparse_model[key])
-        elif isinstance(diff_model[key], torch.Tensor):
-            numpy_array = diff_model[key].numpy()
-            csr = csr_matrix(numpy_array)
-            csr.indices = csr.indices.astype(np.uint16)
-            csr.indptr = csr.indptr.astype(np.uint16)
-            sparse_model[key] = csr
-        elif diff_model[key] is None:
-            sparse_model[key] = None
-
 
 def save_checkpoint(iteration, model, optimizer, opt_param_scheduler,
                     num_floating_point_operations_so_far):
@@ -368,51 +301,72 @@ def save_checkpoint(iteration, model, optimizer, opt_param_scheduler,
             state_dict["rng_state"] = rng_state
 
 
-        # if INTERVALS_SINCE_LAST_BASE_CHECKPOINT == MAX_CACHED_ITERATIONS:
-        #     # we need to store a new base checkpoint
-        #     # we need to reset the ITERATION_SINCE_LAST_BASE_CHECKPOINT to 0 to indicate that we start from a new base checkpoint
-        #     INTERVALS_SINCE_LAST_BASE_CHECKPOINT = 0
-        #     # since we store a base checkpoint, which is the same as storing a normal checkpoint, so just simply set the state_dict to the checkpoint
-        # else: 
-        #     # first we deal with the model part
-        #     # we need to store a sparse checkpoint incrementation
-        #     # we need to first retrieve that old base checkpoint 
-            
-        #     # first obtain the iteration number by subtracting the current iteration with the number of intervals times the save_interval since the last base checkpoint
-        #     iteration = iteration - INTERVALS_SINCE_LAST_BASE_CHECKPOINT * args.save_interval 
-        #     base_ckpt_state_dict = _load_base_checkpoint(args.save, exit_on_missing_checkpoint=True, checkpoint_step=iteration)
+        # performing the bitmask operation
 
-        #     base_model = base_ckpt_state_dict['model']
-        #     new_model = state_dict['model']
+        
+        uncompressed_model = state_dict['model']
+        
+        # get the path of the latest_info.txt first
+        latest_info_path = os.path.join(args.save, "latest_info.txt")
+        
+        latest_base_iteration = 0
+        delta_count = 0
+        
+        # if the file exists here, then we go and fetch the latest_base_iteration and delta_count
+        if os.path.exists(latest_info_path):
+            lines = f.readlines()
+            if len(lines) >= 2:
+                latest_iteration = int(lines[0].strip())
+                latest_base_iteration = int(lines[1].strip())
+                delta_count = int(lines[2].strip())
+        
+        if delta_count >= MAX_CACHED_ITERATIONS or not os.path.exists(latest_info_path):
+            print(f"saving the normal base checkpoint, update the latest base iteration from {latest_base_iteration} to {iteration}")
+            latest_base_iteration = iteration
+            delta_count = 0
+        else:
+            print(f"saving the delta checkpoint, the base iteration is {latest_base_iteration}")
+            checkpoint_path = get_checkpoint_name(args.save, iteration)
+            base_model_path = os.path.join(args.save, checkpoint_path)
+            # if the base model exists, then we can generate the bitmask and save the delta
+            if os.path.exists(base_model_path):
+                base_state_dict = torch.load(base_model_path, map_location='cpu')
+                base_model = base_state_dict['model']
+                new_model = state_dict['model']
+                ################### OPTIMIZATION POINT ###################
+                # Could start another process / daemon to do this
+                diff_model = model_diff(base_model, new_model)
+                
+                # generate the bitmask
+                bitmask = generate_bitmask(diff_model)
+                # Here I wish to modify this: to make the base operation and the delta operation differentiable
+                delta_path = os.path.join(args.save, f"iter_{iteration}.pt")
+
+                sparse_delta_with_bitmask = generate_sparse_delta_with_bitmask(diff_model, bitmask)
+
+                # we only need to change the original model to the sparse_delta_with_bitmask and then store it
+                state_dict['model'] = sparse_delta_with_bitmask
+
+                delta_count += 1
+            else:
+                print(f"the base model does not exist, saving the normal base checkpoint")
+                latest_base_iteration = iteration
+                delta_count = 0
+
+        with open(latest_info_path, "w") as f: 
+            f.write(f"{iteration}\n")
+            f.write(f"{latest_base_iteration}\n")
+            f.write(f"{delta_count}\n")
 
 
-        #     diff_model_state_dict = {}
-        #     model_diff(base_model, new_model, diff_model_state_dict)
-
-        #     sparse_model_state_dict = {}
-        #     sparsification(diff_model_state_dict, sparse_model_state_dict)
-
-        #     state_dict['model'] = sparse_model_state_dict
-
-            # optimizer
 
 
-
-
-
-
-            # now we need to iterate through the diff_model and perform sparsification
-
-
-            # we need to increment the ITERATION_SINCE_LAST_BASE_CHECKPOINT
-            # INTERVALS_SINCE_LAST_BASE_CHECKPOINT += 1
-
-
-
-
-
-
-
+                
+                
+                
+                        
+        
+        # generate the bitmask
 
 
 
--- megatron/sparse_utils.py
+++ megatron/sparse_utils.py
@@ -1,18 +1,209 @@
 import torch
+import numpy as np
 
-def print_info(input):
+def generate_bitmask(delta):
     """
-    This function takes in checkpoint parameters such as model, optimizer, and scheduler and prints out the information of each.
-    The information contains the datatype, the shape, the datastructure
+    This function is used for generating the bitmasks for the delta
     """
-    if isinstance(input, dict):
-        for key, value in input.items():
-            print(f'Key: {key}')
-            print_info(value)
-    elif isinstance(input, list):
-        for i, value in enumerate(input):
-            print(f'Index: {i}')
-            print_info(value)
-    elif isinstance(input, torch.Tensor):
-        print(f'data type: {input.dtype}')
-        print(f'Shape: {input.shape}')
+    bitmasks = {} 
+    for key, value in delta.items():
+        if isinstance(value, dict):
+            bitmasks[key] = {}
+            generate_bitmask(value, bitmasks[key])
+        elif isinstance(value, torch.Tensor):
+            bitmask = (value != 0).to(torch.uint8)  # Use uint8 instead of bool
+            bitmasks[key] = _compress_bitmask(bitmask)
+        elif value is None:
+            bitmasks[key] = None
+        else:
+            raise ValueError(f"Unsupported type for key '{key}': {type(value)}")
+    return bitmasks
+
+
+def _compress_bitmask(bitmask):
+    bitmask = bitmask.view(-1).cpu().numpy()
+    compressed_bitmask = np.packbits(bitmask)
+    return torch.tensor(compressed_bitmask, dtype=torch.uint8)
+
+
+def _decompress_bitmask(compressed_bitmask, original_shape):
+    compressed_bitmask = compressed_bitmask.cpu().numpy()
+    decompressed_bitmask = np.unpackbits(compressed_bitmask)
+    num_bits = np.prod(original_shape)
+    decompressed_bitmask = decompressed_bitmask[:num_bits]
+    return torch.tensor(decompressed_bitmask, dtype=torch.uint8).view(original_shape)
+
+def generate_sparse_delta_with_bitmask(delta, bitmask):
+    """
+    This function is usede for generating the sparse delta with bitmask
+    This function will return a dictionary with the same structure as the delta
+    """
+    
+    def process_delta_and_bitmask(delta, bitmask):
+        sparse_delta_with_bitmask = {}
+        for key in delta.keys():
+            if isinstance(delta[key], dict):
+                sparse_delta_with_bitmask[key] = process_delta_and_bitmask(delta[key], bitmask[key])
+            elif isinstance(delta[key], torch.Tensor):
+                flattened_delta = delta[key].view(-1)
+                flattened_bitmask = _decompress_bitmask(bitmask[key], flattened_delta.shape)
+                sparse_values = flattened_delta[flattened_bitmask == 1]
+                sparse_delta_with_bitmask[key] = (sparse_values, bitmask[key])
+            elif delta[key] is None:
+                sparse_delta_with_bitmask[key] = None
+            else:
+                raise ValueError(f"Unsupported type for key '{key}': {type(delta[key])}")
+        return sparse_delta_with_bitmask
+
+    sparse_delta_with_bitmask = process_delta_and_bitmask(delta, bitmask)
+    return sparse_delta_with_bitmask
+
+def reconstruct_checkpoint_with_bitmask(previous_checkpoint, sparse_delta_with_bitmask):
+    """
+    This function is used for reconstructing the checkpoint with the sparse delta and bitmask
+    @Params: 
+    previous_checkpoint: the checkpoint before applying the sparse delta
+    sparse_delta_with_bitmask: the sparse delta with bitmask
+    """
+    
+    def process_reconstruction(previous_checkpoint, sparse_delta_with_bitmask):
+        new_checkpoint = {}
+        for key in previous_checkpoint.keys():
+            if key in sparse_delta_with_bitmask:
+                if isinstance(sparse_delta_with_bitmask[key], tuple):
+                    sparse_values, compressed_bitmask = sparse_delta_with_bitmask[key]
+                    flattened_bitmask = _decompress_bitmask(compressed_bitmask, previous_checkpoint[key].view(-1).shape)
+                    delta = torch.zeros_like(previous_checkpoint[key]).view(-1)
+                    delta[flattened_bitmask == 1] = sparse_values
+                    delta = delta.view(previous_checkpoint[key].shape)
+                    new_checkpoint[key] = previous_checkpoint[key] + delta
+                elif isinstance(sparse_delta_with_bitmask[key], dict):
+                    new_checkpoint[key] = process_reconstruction(previous_checkpoint[key], sparse_delta_with_bitmask[key])
+                elif sparse_delta_with_bitmask[key] is None:
+                    new_checkpoint[key] = previous_checkpoint[key]
+                else:
+                    raise ValueError(f"Unsupported type for key '{key}' in sparse_delta_with_bitmask: {type(sparse_delta_with_bitmask[key])}")
+            else:
+                new_checkpoint[key] = previous_checkpoint[key]
+        return new_checkpoint
+
+    return process_reconstruction(previous_checkpoint, sparse_delta_with_bitmask)
+
+def model_diff(base_model, new_model):
+    """
+    This function is used to compute the difference between the base model and the new model
+    """
+    diff_state_dict = {}
+    for key in base_model.keys():
+        if key in new_model.keys():
+            if isinstance(base_model[key], dict) and isinstance(new_model[key], dict):
+                diff_state_dict[key] = {}
+                model_diff(base_model[key], new_model[key], diff_state_dict[key])
+            elif isinstance(base_model[key], torch.Tensor) and isinstance(new_model[key], torch.Tensor):
+                diff_state_dict[key] = new_model[key] - base_model[key]
+            elif base_model[key] is None and new_model[key] is None:
+                diff_state_dict[key] = None
+            else:
+                raise ValueError(f"Mismatched types for key '{key}' in base and new model, base has the type {type(base_model[key])} and new has the type {type(new_model[key])}")
+        else:
+            raise ValueError(f"Key '{key}' not found in new model")
+    return diff_state_dict
+
+
+
+
+
+
+
+
+
+
+
+
+def compress_indices(indices, dtype=torch.int32):
+    # Compress indices to a specified lower-bit integer type
+    return indices.to(dtype)
+
+def decompress_indices(indices, dtype=torch.int64):
+    # Decompress indices back to int64
+    return indices.to(dtype)
+
+
+# sparsification using traditional COO sparse matrix
+def sparsification(diff_model, sparse_model, device='cuda'):
+    """
+    This function is used to perform sparsification on the diff_model
+    We need to iterate through the diff_model and perform sparsification on the tensors
+    """
+    for key in diff_model:
+        if isinstance(diff_model[key], dict):
+            sparse_model[key] = {}
+            sparsification(diff_model[key], sparse_model[key], device)
+        elif isinstance(diff_model[key], torch.Tensor):
+            # Perform sparsification using COO format
+            tensor = diff_model[key].to(device)
+            tensor_sparse = tensor.to_sparse()
+            indices = tensor_sparse.indices()
+            values = tensor_sparse.values()
+            # Compress indices for storage or transmission
+
+            # Here note that the latest pyTorch(torch 2.3.0) could apply torch.uint16 to directly convert the datatype of the indices to uint16
+            # This is ideal since the indices tensor does not have to go through CPU to be converted to nupmy array
+            # However now with torch 2.2.0 we have no choice but to convert to numpy array and then use uint 16
+            
+            
+            # compressed_indices = indices.to(torch.uint16)
+            # compressed_indices = compress_indices(indices, dtype=torch.uint16)
+            
+            indices_numpy = indices.cpu().numpy()
+            compressed_indices = np.array(indices_numpy, dtype=np.uint16)
+
+            # Store the compressed indices and values
+            sparse_model[key] = {
+                "size": tensor_sparse.size(),
+                "compressed_indices": compressed_indices,
+                "values": values
+            }
+
+        elif diff_model[key] is None:
+            sparse_model[key] = None
+
+def de_sparsification(sparse_model, diff_model):
+    for key in sparse_model:
+        if isinstance(sparse_model[key], dict) and "compressed_indices" in sparse_model[key]:
+            compressed_indices_uint16 = sparse_model[key]["compressed_indices"]
+            values = sparse_model[key]["values"]
+            size = sparse_model[key]["size"]
+            compress_indices_int64 = np.array(compressed_indices_uint16, dtype=np.int64)
+            indices = torch.tensor(compress_indices_int64, dtype=torch.int64)
+            indices = indices.to(values.device)
+
+            tensor = torch.sparse_coo_tensor(indices, values, size, device=values.device)
+            diff_model[key] = tensor.to_dense()
+        elif isinstance(sparse_model[key], dict):
+            diff_model[key] = {}
+            de_sparsification(sparse_model[key], diff_model[key])
+        elif sparse_model[key] is None:
+            diff_model[key] = None
+
+
+
+def incrementation(base_model, diff_model):
+    recovered_ckpt = {}
+    for key in base_model:
+        if key in diff_model:
+            if isinstance(base_model[key], dict) and isinstance(diff_model[key], dict):
+                recovered_ckpt[key] = incrementation(base_model[key], diff_model[key])
+            elif isinstance(base_model[key], torch.Tensor) and isinstance(diff_model[key], torch.Tensor):
+                base_tensor = base_model[key].to(diff_model[key].device)
+                recovered_ckpt[key] = base_tensor + diff_model[key]
+            elif base_model[key] is None and diff_model[key] is None:
+                recovered_ckpt[key] = None
+            else:
+                raise ValueError(f"Mismatched types for key '{key}' in base and diff model, base has the type {type(base_model[key])} and diff has the type {type(diff_model[key])}")
+        else:
+            raise ValueError(f"Key '{key}' not found in diff model")
+    return recovered_ckpt
+
+
+    
